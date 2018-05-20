01b83a3c8d5456ac1df8361cf283c757ea7a77da
Chinmay Garde <chinmaygarde@gmail.com>
2018-05-19 19:11:27 -0700

    Add support for Linux hosts.

--- tools/setup_sdk.sh
+++ tools/setup_sdk.sh
@@ -2,6 +2,21 @@
 
 set -e
 
+ReportError() {
+	echo "$@" 1>&2;
+}
+
+TOOCHAIN_NAME=""
+
+if [ "$(uname)" == "Darwin" ]; then
+    TOOLCHAIN_NAME="darwin-x64"
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+	TOOLCHAIN_NAME="linux-x64"
+else
+	ReportError "This platform is not supported. Supported platforms are Darwin or Linux."
+	exit -1
+fi
+
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 SDK_DIR="${SCRIPT_DIR}/../sdk/"
 
@@ -9,10 +24,10 @@ rm -rf "${SDK_DIR}"
 mkdir -p "${SDK_DIR}"
 
 echo "Settng up Toolchain..."
-wget -O "${SDK_DIR}/toolchain.tar.gz" https://storage.googleapis.com/xcompile_sdk/pi/toolchain/darwin_x64.tar.gz
+wget -O "${SDK_DIR}/toolchain.tar.gz" https://storage.googleapis.com/xcompile_sdk/pi/toolchain/${TOOLCHAIN_NAME}.tar.gz
 tar -xzvf "${SDK_DIR}/toolchain.tar.gz" -C "${SDK_DIR}"
 rm "${SDK_DIR}/toolchain.tar.gz"
-mv "${SDK_DIR}/darwin-x64" "${SDK_DIR}/toolchain"
+mv "${SDK_DIR}/${TOOLCHAIN_NAME}" "${SDK_DIR}/toolchain"
 
 echo "Setting up Sysroot..."
 wget -O "${SDK_DIR}/sysroot.tar.gz" https://storage.googleapis.com/xcompile_sdk/pi/sysroot.tar.gz
